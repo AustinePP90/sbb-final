081ec48bc70a38ac8dce227aef38c77ae94c5d80
Austine <chucky891213@naver.com>
2024-11-01 10:25:31 +0900

    MR: 회원가입(PasswordEncoder) 작업

--- src/main/java/com/mysite/sbbfinal/controller/HomeController.java
+++ src/main/java/com/mysite/sbbfinal/controller/HomeController.java
@@ -1,10 +1,24 @@
 package com.mysite.sbbfinal.controller;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.mysite.sbbfinal.domain.User;
+import com.mysite.sbbfinal.mapper.UserMapper;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+	
+	private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 	
 	@GetMapping({"/", "/home"})
 	public String home() {
@@ -20,5 +34,25 @@ public class HomeController {
     @GetMapping("/signup")
     public String signupForm() {
         return "signup";
+    }
+    
+    // 회원가입 처리
+    @PostMapping("/signup")
+    public String signup(@RequestParam("username") String username,
+                         @RequestParam("password") String password) {
+
+        // 사용자 생성
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password)); // 비밀번호 인코드
+        user.setEnabled(true);
+
+        // 사용자 저장
+        userMapper.save(user);
+
+        // ROLE_USER(기본 권한) 추가 (roles 테이블에서 ROLE_USER의 id가 1이라고 가정)
+        userMapper.insertUserRole(user.getId(), 1L);
+
+        return "redirect:/login";
     }
 }
--- src/main/java/com/mysite/sbbfinal/mapper/UserMapper.java
+++ src/main/java/com/mysite/sbbfinal/mapper/UserMapper.java
@@ -8,4 +8,8 @@ import com.mysite.sbbfinal.domain.User;
 @Mapper
 public interface UserMapper {
 	User findByUsername(@Param("username") String username);
+	
+    void save(User user);
+    
+    void insertUserRole(@Param("userId") Long userId, @Param("roleId") Long roleId);
 }
